0494c1cc68c368e7cbc1757f11c9e8c3bfccc2f8
Arvind <144233717+arvind-mangalarapu@users.noreply.github.com>
2024-08-02 17:27:38 +0530

    added deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,8 @@
 	import WeatherApi from '$lib/components/WeatherApi.svelte';
 	import CurrentLocation from '$lib/components/CurrentLocation.svelte';
 	import WeatherUi from '$lib/components/WeatherUi.svelte';
-	import WeatherSearch from '$lib/components/WeatherSearch.svelte';
 </script>
 
 <WeatherApi />
 <CurrentLocation />
 <WeatherUi />
-<!-- <WeatherSearch /> -->
